c4e55751b0d0fd360ed6c53edcd11912852f587e
dc <dcrampton@gmail.com>
2019-03-04 21:26:24 -0800

    more pitchscript

--- pitch.py
+++ pitch.py
@@ -1,24 +1,39 @@
 import os
 import sys
-
 sys.path.append(os.path.abspath("pydub"))
-
 from pydub import AudioSegment
 from pydub.playback import play
+from pathlib import Path
 
-sound = AudioSegment.from_file('HitsoundQuakeChampions.wav', format="wav")
+soundsPath = Path("./Sounds/")
+for soundPath in soundsPath.glob('**/*_src.wav'):
+  suffix = str(soundPath.suffix)
+  soundName = str(soundPath.stem.replace("_src",""))
+  folder = str(soundPath.parent)
 
-octaves_high = 1.75
-octaves_low = -1.75
+  midName = folder + "/" + soundName + "_mid"
+  lowName = folder + "/" + soundName + "_low"
+  highName = folder + "/" + soundName + "_high"
 
+  print("Writing " + soundName + " to " + folder)
+  print("...Mid")
+  sound = AudioSegment.from_file(soundPath, format="wav")
+  sound.export(midName, format="wav")
 
-sample_high = int(sound.frame_rate * (2.0 ** octaves_high))
+  octaves_high = 1.75
+  octaves_low = -1.75
 
-sound_high = sound._spawn(sound.raw_data, overrides={'frame_rate': sample_high})
-sound_high = sound_high.set_frame_rate(sound.frame_rate)
-sound_high.export("HitsoundQuakeChampionsHigh.wav", format="wav")
+  sample_high = int(sound.frame_rate * (2.0 ** octaves_high))
 
-sample_low = int(sound.frame_rate * (2.0 ** octaves_low))
-sound_low = sound._spawn(sound.raw_data, overrides={'frame_rate': sample_low})
-sound_low = sound_low.set_frame_rate(sound.frame_rate)
-sound_low.export("HitsoundQuakeChampionsLow.wav", format="wav")
+  print("...Low")
+  sample_low = int(sound.frame_rate * (2.0 ** octaves_low))
+  sound_low = sound._spawn(sound.raw_data, overrides={'frame_rate': sample_low})
+  sound_low = sound_low.set_frame_rate(sound.frame_rate)
+  sound_low.export(lowName, format="wav")
+
+  print("...High")
+  sound_high = sound._spawn(sound.raw_data, overrides={'frame_rate': sample_high})
+  sound_high = sound_high.set_frame_rate(sound.frame_rate)
+  sound_high.export(highName, format="wav")
+
+  print("---")
